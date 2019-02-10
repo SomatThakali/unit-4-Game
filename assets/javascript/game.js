0303f426fdfa3687553e7444ec91017b8785024c
Somat Thakali <sthakal000@citymail.cuny.edu>
2019-02-09 19:36:17 -0500

    Removed unwanted html tags

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+
+$(document).ready (function(){
+
+
+// Objects that holds the properties of each characters
+var characters = {
+    somat:{
+        name: 'somat',
+        healthPoints : 120,
+        attackPower: 6,
+        CounterAttackPower: 20,
+        imageUrl: "assets/images/1.jpg"
+    },
+
+    michael:{
+        name: 'michael',
+        healthPoints : 100,
+        attackPower: 10,
+        CounterAttackPower: 15,
+        imageUrl: "assets/images/2.jpg"
+    },
+
+   jamal:{
+        name: 'jamal',
+        healthPoints : 110,
+        attackPower: 20,
+        CounterAttackPower: 5,
+        imageUrl: "assets/images/3.jpg"
+    },
+
+
+    peter:{
+        name: 'peter',
+        healthPoints : 150,
+        attackPower: 5,
+        CounterAttackPower: 25,
+        imageUrl: "assets/images/3.jpg"
+    },
+      
+}; // end of the object
+
+
+var yourCharacter;
+});
